5158d555e484ad974dff8a43957430aa7794df20
Kamil <203901@edu.p.lodz.pl>
2018-02-28 22:08:42 +0100

    Initial commit

---
+++ src/main/java/pl/ksr/App.java
@@ -0,0 +1,9 @@
+package pl.ksr;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/pl/ksr/model/ArticleWithPlace.java
@@ -0,0 +1,10 @@
+package pl.ksr.model;
+
+import lombok.*;
+
+@Data
+public class ArticleWithPlace {
+
+    private String place;
+    private String article;
+}
