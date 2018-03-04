ec8be65890f0709afcb621212b3f4dc97f4bbf9d
Kamil <203901@edu.p.lodz.pl>
2018-03-04 13:11:12 +0100

    Read data from file with defined places.

--- src/main/java/pl/ksr/App.java
+++ src/main/java/pl/ksr/App.java
@@ -1,25 +1,19 @@
 package pl.ksr;
 
 import pl.ksr.model.ArticleWithPlace;
-import pl.ksr.services.ConverterService;
 import pl.ksr.services.FileService;
+import pl.ksr.services.TestDataService;
 
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-
-        // todo: need to remove reuters without places or articles
-
-        // todo: read one by one element
-
         String doc = FileService.readFile("reut2-000.sgm");
-        ArticleWithPlace[] articlesWithPlaces = ConverterService.getDocumentInfo(doc);
+        ArticleWithPlace[] articlesWithPlaces =
+                TestDataService.getData("reut2-000.sgm", new String[]{"usa", "argentina"});
 
         for (ArticleWithPlace awp : articlesWithPlaces) {
             System.out.println(awp);
         }
-
-
     }
 }
---
+++ src/main/java/pl/ksr/services/TestDataService.java
@@ -0,0 +1,61 @@
+package pl.ksr.services;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import pl.ksr.model.ArticleWithPlace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// reading data from file
+public class TestDataService {
+
+    // getting array of ArticleWithPlace with places in param, path to file
+    public static ArticleWithPlace[] getData(String path, String[] places) {
+        List<ArticleWithPlace> articleWithPlaces = new ArrayList<>();
+        String data = prepareFile(path);
+
+        Document doc = Jsoup.parse(data);
+        Elements reuters = doc.select("reuters");
+
+        for (Element reuter : reuters) {
+            String place;
+            String article;
+
+            try {
+                place = reuter.selectFirst("places").text();
+                article = reuter.selectFirst("article").text();
+            } catch (NullPointerException e) {
+                continue;
+            }
+
+            boolean isPlaceCorrect = false;
+            for (String placeParam : places) {
+                if (placeParam.equals(place)) {
+                    isPlaceCorrect = true;
+                    break;
+                }
+            }
+
+            if (isPlaceCorrect)
+                articleWithPlaces.add(new ArticleWithPlace(place, article));
+        }
+
+        return articleWithPlaces.toArray(new ArticleWithPlace[0]);
+    }
+
+    // reading file and changing tags <body> to <article> (tags <body> don't work as other>
+    private static String prepareFile(String path) {
+        String document = FileService.readFile(path);
+        return validateDocument(document);
+    }
+
+    // changing tags body to article
+    private static String validateDocument(String document) {
+        return document
+                .replace("<BODY>", "<ARTICLE>")
+                .replace("</BODY>", "</ARTICLE>");
+    }
+}
