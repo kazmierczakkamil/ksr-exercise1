92ab86a9c9b33082c5a2eebf8302786922bf9565
Kamil <203901@edu.p.lodz.pl>
2018-03-19 14:29:06 +0100

    Small fixes in main

--- src/main/java/pl/ksr/App.java
+++ src/main/java/pl/ksr/App.java
@@ -1,38 +1,26 @@
 package pl.ksr;
 
-import edu.stanford.nlp.simple.Sentence;
+import pl.ksr.model.Article;
+import pl.ksr.model.LabelType;
+import pl.ksr.services.TestDataService;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class App {
 
-
-
-
     public static void main(String[] args) throws IOException {
-//        List<Article> articlesWithPlaces = new ArrayList<Article>();
-//        for(int i=0; i<=21; ++i) {
-//            articlesWithPlaces.addAll(
-//                TestDataService.getData("reut2-"+String.format("%03d", i)+".sgm",
-//                                        new String[]{"usa", "argentina"}, LabelType.PLACE));
-//    }
-//
-//        for (Article awp : articlesWithPlaces) {
-//            System.out.println(awp);
-//        }
-
-        String string = "cars movies training trains eggs ate eaten moving car ";
-
-        for (String val : getLemmasList(string)) {
-            System.out.println(val);
+        List<Article> articlesWithPlaces = new ArrayList<Article>();
+        for (int i = 0; i <= 21; ++i) {
+            articlesWithPlaces.addAll(
+                    TestDataService.getData("reut2-" + String.format("%03d", i) + ".sgm",
+                            new String[]{"usa", "argentina"}, LabelType.PLACE));
         }
 
-    }
-
-    public static List<String> getLemmasList(String text) {
-        Sentence sentence = new Sentence(text);
-        return sentence.lemmas();
+        for (Article awp : articlesWithPlaces) {
+            System.out.println(awp);
+        }
     }
 
 }
