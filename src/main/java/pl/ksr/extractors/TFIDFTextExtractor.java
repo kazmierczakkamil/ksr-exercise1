b44d3ca273f267255882eb808045ffbf6a0e98d8
afoltynski <endixlol@gmail.com>
2018-03-26 17:45:09 +0200

    Optimized tfidf

--- src/main/java/pl/ksr/extractors/TFIDFTextExtractor.java
+++ src/main/java/pl/ksr/extractors/TFIDFTextExtractor.java
@@ -5,11 +5,14 @@ import pl.ksr.model.ExtractedData;
 import pl.ksr.services.Lemmatizer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TFIDFTextExtractor {
     public static List<ExtractedData> extractFrom(List<Article> articles) {
         List<ExtractedData> extractedData = new ArrayList<>();
+        Map<String,Double> occurrencesInAllDocuments = new HashMap<>();
         ExtractedData tmp = null;
         for(Article article : articles){
             String wordsBeforeLammatization = article.articleBody
@@ -21,29 +24,35 @@ public class TFIDFTextExtractor {
                 if(tmp.features.containsKey(word))
                     tmp.features.put(word,tmp.features.get(word)+1);
                 else tmp.features.put(word,1.0);
+                if(!occurrencesInAllDocuments.containsKey(word))
+                    occurrencesInAllDocuments.put(word,1.0);
+                else if(!tmp.features.containsKey(word))
+                    occurrencesInAllDocuments.put(word,occurrencesInAllDocuments.get(word)+1.0);
             }
             extractedData.add(tmp);
         }
 
         int amountOfDocuments = extractedData.size();
         int i = 0;
+
+        long startTime = System.currentTimeMillis();
         for (ExtractedData document : extractedData) {
             int allTermOccurrences = document.features.values().parallelStream()
                     .reduce(0.0, Double::sum)
                     .intValue();
 
-            long startTime = System.currentTimeMillis();
             for (String word : document.features.keySet()) {
-                long inHowManyDocumentsWordExists =
-                        extractedData.parallelStream()
-                                .filter(e -> e.features.keySet().contains(word))
-                                .count();
-                double idf = Math.log(amountOfDocuments / inHowManyDocumentsWordExists);
+//                long inHowManyDocumentsWordExists =
+//                        extractedData.parallelStream()
+//                                .filter(e -> e.features.keySet().contains(word))
+//                                .count();
+                //double idf = Math.log(amountOfDocuments / inHowManyDocumentsWordExists);
+                double idf = Math.log(amountOfDocuments / occurrencesInAllDocuments.get(word));
                 document.features.compute(word, (key, value) -> value / allTermOccurrences * idf);
             }
-            System.out.println("Count occurences: " + (System.currentTimeMillis() - startTime));
             i++;
-            System.out.println(i);
+            System.out.print(i);
+            System.out.println(" idf: " + (System.currentTimeMillis() - startTime));
         }
         return extractedData;
     }
