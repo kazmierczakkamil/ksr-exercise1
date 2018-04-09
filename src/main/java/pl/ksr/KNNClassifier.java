4999827f8e1da179e65717e9c48c55f3c2684dcb
afoltynski <endixlol@gmail.com>
2018-04-09 13:17:32 +0200

    removed redundant code

--- src/main/java/pl/ksr/KNNClassifier.java
+++ src/main/java/pl/ksr/KNNClassifier.java
@@ -41,33 +41,6 @@ public class KNNClassifier {
             resultData.setAssignedLabel(label);
             results.add(resultData);
         });
-
-        //======================
-
-
-//        int i = 0;
-//        for (ExtractedData documentToClassify : testData) {
-//            ResultData resultData = new ResultData(documentToClassify);
-//
-//            Map<ExtractedData, Double> nearestNeighbors = new HashMap<>();
-//
-//            for (ExtractedData dataToCountDistance : trainingData) {
-//                double distance = metric.getDistance(dataToCountDistance, documentToClassify);
-//                nearestNeighbors.put(dataToCountDistance, distance);
-//            }
-//            List<String> labelsOfNearestNeighbors = nearestNeighbors.entrySet().stream()
-//                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-//                    .limit(k)
-//                    .map(e -> e.getKey().label)
-//                    .collect(Collectors.toList());
-//
-//            String label = Seq.of(labelsOfNearestNeighbors).mode().get().get(0);
-//            resultData.setAssignedLabel(label);
-//            results.add(resultData);
-//
-//            System.out.println("Classified documents: " + i);
-//            i++;
-//        }
         return results;
     }
 }
